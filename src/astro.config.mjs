7a644d222ad9a3e6f15eac2f5aa28fb6c0b0e144
EdamAme <121654029+EdamAme-x@users.noreply.github.com>
2023-11-26 14:24:25 +0900

    astro on deno

---
+++ src/astro.config.mjs
@@ -0,0 +1,8 @@
+import { defineConfig } from 'npm:astro/config';
+import deno from "npm:@astrojs/deno@4.2.0";
+
+// https://astro.build/config
+export default defineConfig({
+    adapter: deno(),
+    output: 'server',
+});
